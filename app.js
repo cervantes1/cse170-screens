7ab9fb559c51f5e9cf38a2c9344a35f3fb9907cd
cervantes1 <rgcervan@ucsd.edu>
2020-02-13 01:51:56 -0800

    js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-
-/**
+/** 
  * Module dependencies.
  */
+
 
 var express = require('express');
 var http = require('http');
@@ -41,3 +41,54 @@ app.get('/', index.view);
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+
+
+
+
+
+var temperature = document.querySelector(".temperature-value p");
+var location = document.querySelector(".location p");
+
+var weather = {};
+
+
+
+var KELVIN = 273;
+var key = "5a5c31b9c3adb1179cfecf9a4400afe0"; 
+
+
+function setPosition(){
+  let lat = 32.8801;
+  let long = 117.2340;
+
+  getWeather(lat, long);
+}
+
+
+function getWeather(lat, long){
+  let api = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${key}`;
+  console.log(api);
+
+  fetch(api)
+    .then(function(response){
+      let data = response.json();
+      return data;
+    })
+    .then(function(data){
+      weather.temperature.value = Math.floor(data.main.temp) - KELVIN;
+      weather.description = data.weather[0].description;
+    })
+    .then(function(){
+      displayWeather();
+    });
+}
+
+
+
+
+
+function displayWeather(){
+  temperature.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+}
+
